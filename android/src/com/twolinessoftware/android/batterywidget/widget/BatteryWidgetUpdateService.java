073156d2b684489fae1670336e72c652f4899d92
John Carpenter <jcarpent@gmail.com>
2011-05-31 18:38:15 -0600

    Added a settings activity to allow the user to configure the widget. Widget can be configured for colour and size (One of three sizes). Updated version in the manifest to 1.1 version code 2.

--- android/src/com/twolinessoftware/android/batterywidget/widget/BatteryWidgetUpdateService.java
+++ android/src/com/twolinessoftware/android/batterywidget/widget/BatteryWidgetUpdateService.java
@@ -15,6 +15,7 @@
  */
 package com.twolinessoftware.android.batterywidget.widget;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
@@ -22,41 +23,52 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Bitmap.Config;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.BatteryManager;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
+import com.twolinessoftware.android.batterywidget.Defaults;
 import com.twolinessoftware.android.batterywidget.R;
+import com.twolinessoftware.android.batterywidget.SettingsActivity;
 
-
-
-public class BatteryWidgetUpdateService extends Service{
+public class BatteryWidgetUpdateService extends Service {
 
 	private static final String LOGNAME = "BatteryWidgetUpdateService";
 	private int level;
 
-	BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver(){
-	    @Override
-	    public void onReceive(Context context, Intent intent){
-	        int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-	        int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-	        if (rawlevel >= 0 && scale > 0) {
-	            level = (rawlevel * 100) / scale;
-	        }
-	 
-	    	updateWidget();
-	    }
+	BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+			int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+			if (rawlevel >= 0 && scale > 0) {
+				level = (rawlevel * 100) / scale;
+			}
+
+			updateWidget();
+		}
 	};
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		startUp(intent);
 		return START_STICKY;
 	}
-	
+
 	@Override
 	public void onStart(Intent intent, int startId) {
 		startUp(intent);
@@ -65,9 +77,9 @@ public class BatteryWidgetUpdateService extends Service{
 	private void startUp(Intent intent) {
 
 		Log.d(LOGNAME, "Updating Widget");
-		
-		registerListeners(); 
- 
+
+		registerListeners();
+
 	}
 
 	@Override
@@ -77,19 +89,60 @@ public class BatteryWidgetUpdateService extends Service{
 	}
 
 	private void registerListeners() {
-		
-		IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+
+		IntentFilter batteryLevelFilter = new IntentFilter(
+				Intent.ACTION_BATTERY_CHANGED);
 		registerReceiver(batteryLevelReceiver, batteryLevelFilter);
 	}
 
 	private void updateWidget() {
-		
-		RemoteViews v = new RemoteViews(getPackageName(),R.layout.battery_widget);
-			
-		v.setTextViewText(R.id.textBatteryLevel, level+"/100");
-		
-		v.setProgressBar(R.id.progressBarBattery,100, level, false);
-		
+
+		RemoteViews v = new RemoteViews(getPackageName(),
+				R.layout.battery_widget);
+
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		int color = sp.getInt(Defaults.COLOR_PREFERENCE, Color.WHITE);
+
+		v.setTextColor(R.id.textBatteryLevelSm, color);
+		v.setTextViewText(R.id.textBatteryLevelSm, level + "/100");
+
+		v.setTextColor(R.id.textBatteryLevelMed, color);
+		v.setTextViewText(R.id.textBatteryLevelMed, level + "/100");
+
+		v.setTextColor(R.id.textBatteryLevelLg, color);
+		v.setTextViewText(R.id.textBatteryLevelLg, level + "/100");
+
+		String fontSize = sp.getString(Defaults.FONTSIZE_PREFERENCE,
+				getResources().getStringArray(R.array.fontSizesValues)[0]);
+		if (fontSize.equals("sm")) {
+			v.setViewVisibility(R.id.textBatteryLevelSm, View.VISIBLE);
+			v.setViewVisibility(R.id.textBatteryLevelMed, View.GONE);
+			v.setViewVisibility(R.id.textBatteryLevelLg, View.GONE);
+		} else if (fontSize.equals("med")) {
+			v.setViewVisibility(R.id.textBatteryLevelSm, View.GONE);
+			v.setViewVisibility(R.id.textBatteryLevelMed, View.VISIBLE);
+			v.setViewVisibility(R.id.textBatteryLevelLg, View.GONE);
+		} else {
+			v.setViewVisibility(R.id.textBatteryLevelSm, View.GONE);
+			v.setViewVisibility(R.id.textBatteryLevelMed, View.GONE);
+			v.setViewVisibility(R.id.textBatteryLevelLg, View.VISIBLE);
+		}
+
+		v.setInt(R.id.separator, "setBackgroundColor", color);
+
+		// v.setProgressBar(R.id.progressBarBattery, 100, level, false);
+		//Bitmap imageIndicator = BitmapFactory.decodeResource(getResources(),
+		//		R.drawable.level_indicator);
+		//v.setImageViewBitmap(R.id.progressBarBattery,proxy);
+		v.setInt(R.id.progressBarBattery, "setColorFilter", color);
+
+		Intent intent = new Intent(this, SettingsActivity.class);
+
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				intent, 0);
+		v.setOnClickPendingIntent(R.id.layoutBatteryContainer, pendingIntent);
+
 		// Push update for this widget to the home screen
 		ComponentName batteryWidget = new ComponentName(this,
 				BatteryWidget.class);
@@ -99,13 +152,9 @@ public class BatteryWidgetUpdateService extends Service{
 		stopSelf();
 	}
 
-	
-	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
 
-	
-	
 }
